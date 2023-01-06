6150720581bfb50324e8f91917017e64e3860cd4
ekaterina_mayatskaya <mayatskaya.katya@mail.ru>
2023-01-06 12:42:35 +0300

    Add comments in tests

--- test_main_.py
+++ test_main_.py
@@ -2,19 +2,26 @@ import main_
 import pytest
 import imghdr
 
+
+# Ввод чисел, вместо текста приводит к ошибке ValueError
 def test_error():
     with pytest.raises(ValueError):
         main_.text_sum(12323434)
 
+
+# Проверка, что вывод первой модели (суммаризации текста) - текст
 def test_read_sum():
     summ = main_.predict_text('Videos that say approved vaccines are dangerous and cause autism, cancer or infertility are among those that will be taken down, the company said.  The policy includes the termination of accounts of anti-vaccine influencers.  Tech giants have been criticised for not doing more to counter false health information on their sites.  In July, US President Joe Biden said social media platforms were largely responsible for peoples scepticism in getting vaccinated by spreading misinformation, and appealed for them to address the issue.')
     assert type(summ) == str
 
+
+# Проверка, если пользователь не ввел текст, то модель будет работать с дефолтным текстом, результат работы - текст
 def test_null():
     summ = main_.predict_text("")
     assert type(summ) == str
 
-# проверим, что на выходе картинка
+
+# Проверка, что вторая модель выдает результат свой работы в формате "jpeg", и также работает с дефолтным текстом
 def test_kl():
     result = main_.generate_image("")
     assert imghdr.what(result) == "jpeg"
