8d5b689b60fa0f46567604268b941bc22accbbf3
ekaterina_mayatskaya <mayatskaya.katya@mail.ru>
2023-01-06 11:04:40 +0300

    Add changes in main.py

--- main_.py
+++ main_.py
@@ -19,3 +19,26 @@ def generate_image(text):
     else:
         return picture_gen('Pink elephant')
 
+if __name__ == '__main__':
+    with gr.Blocks() as demo:
+        with gr.Row():
+            with gr.Column(scale=2):
+                pass
+            with gr.Column(scale=3, elem_id='main_col'):
+                with gr.Row():
+                    gr.Markdown(
+                        """### The meaning in one picture!""")
+                with gr.Row():
+                    text_input = gr.Text(label='Enter your text here:', elem_id='text_input')
+                    text_output = gr.Text(label='Text summary here', elem_id='text_output', interactive=False)
+                with gr.Row():
+                    get_text = gr.Button("Get summary", elem_id='button2')
+                    image_button = gr.Button("Generate picture", elem_id='button2')
+                with gr.Row():
+                    image_output = gr.Image(label='Result', show_label=True, elem_id='image')
+            with gr.Column(scale=2):
+                pass
+
+        get_text.click(predict_text, inputs=text_input, outputs=text_output)
+        image_button.click(generate_image, inputs=text_output, outputs=image_output)
+    demo.launch(server_name='0.0.0.0')
